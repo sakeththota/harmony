970c550676c618bc621fd46d748f13658ef86185
Saketh Thota <saketht@umich.edu>
2023-02-17 00:34:44 -0600

    added icons, color pallette, and finished login ui

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,19 @@
 module.exports = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {}
+		extend: {
+			colors: {
+				primary: '#5E81AC',
+				secondary: '#81a1c1',
+				accent: '#88C0D0',
+				neutral: '#D8DEE9',
+				'base-100': '#141416',
+				info: '#b48ead',
+				success: '#a3be8c',
+				warning: '#ebcb8b',
+				error: '#bf616a'
+			}
+		}
 	},
 	plugins: []
 };
