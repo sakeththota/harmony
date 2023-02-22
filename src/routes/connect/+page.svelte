6b579c7e7a071e9c2242e053e950212c12cf5cfb
Saketh Thota <saketht@umich.edu>
2023-02-21 21:46:32 -0600

    added prisma and now setting cookie for connected spotify user

--- src/routes/connect/+page.svelte
+++ src/routes/connect/+page.svelte
@@ -6,14 +6,14 @@
 	<p class="text-2xl font-bold text-neutral">Connect Music</p>
 	<p class="text-neutral text-sm">Connect your favorite music services to start listening!</p>
 	<form method="POST" class="flex flex-row gap-4 w-full pt-2">
-		<ConnectProviderCard label="Spotify" icon="logos:spotify-icon" connected={true}>
-			<span slot="description">some description</span>
+		<ConnectProviderCard label="Spotify" icon="logos:spotify-icon" connected={false}>
+			<!-- <span slot="description">some description</span> -->
 		</ConnectProviderCard>
 		<ConnectProviderCard label="Soundcloud" icon="logos:soundcloud" connected={false}>
-			<span slot="description">some description</span>
+			<!-- <span slot="description">some description</span> -->
 		</ConnectProviderCard>
 		<ConnectProviderCard label="Youtube Music" icon="logos:youtube-icon" connected={null}>
-			<span slot="description">some description</span>
+			<!-- <span slot="description">some description</span> -->
 		</ConnectProviderCard>
 	</form>
 </div>
--- src/routes/connect/spotify/+server.ts
+++ src/routes/connect/spotify/+server.ts
@@ -1,28 +1,48 @@
-import { error, redirect, type RequestHandler } from '@sveltejs/kit';
+import { error, fail, redirect, type RequestHandler } from '@sveltejs/kit';
 import { PUBLIC_SPOTIFY_CLIENT_ID } from '$env/static/public';
 import { SPOTIFY_CLIENT_SECRET } from '$env/static/private';
 
-export const GET: RequestHandler = async ({ url, locals, fetch }) => {
-	const code = url.searchParams.get('code') || null;
-	// const state = url.searchParams.get('state') || null;
-	if (!code) throw error(400, 'Null authorization code.');
-	if (!locals.session) throw error(40, 'No harmony user logged in');
+const getSpotifyAccessToken = async (code: string) => {
+	try {
+		const data = await fetch('https://accounts.spotify.com/api/token', {
+			body: new URLSearchParams({
+				grant_type: 'authorization_code',
+				code: code,
+				redirect_uri: 'http://localhost:5173/connect/spotify'
+			}),
+			headers: {
+				Authorization: `Basic ${Buffer.from(
+					`${PUBLIC_SPOTIFY_CLIENT_ID}:${SPOTIFY_CLIENT_SECRET}`
+				).toString('base64')}`,
+				'Content-Type': 'application/x-www-form-urlencoded'
+			},
+			method: 'POST'
+		}).then(async (data) => await data.json());
+		return { data, error: null };
+	} catch (error) {
+		console.error(error);
+		return { data: null, error };
+	}
+};
 
-	fetch('https://accounts.spotify.com/api/token', {
-		body: new URLSearchParams({
-			grant_type: 'authorization_code',
-			code: code,
-			redirect_uri: 'http://localhost:5173/connect'
-		}),
-		headers: {
-			Authorization: `Basic ${Buffer.from(
-				`${PUBLIC_SPOTIFY_CLIENT_ID}:${SPOTIFY_CLIENT_SECRET}`
-			).toString('base64')}`,
-			'Content-Type': 'application/x-www-form-urlencoded'
-		},
-		method: 'POST'
-	}).then((data) => {
-		console.log(data.body);
+export const GET: RequestHandler = async ({ url, locals, cookies }) => {
+	const code = url.searchParams.get('code') || null;
+
+	if (!code) throw error(400, 'Null authorization code.');
+	if (!locals.session) throw error(400, 'No harmony user logged in');
+
+	const { data, error: err } = await getSpotifyAccessToken(code);
+
+	if (err) {
+		return fail(500, { error: 'Something went wrong getting access to Spotify :(' });
+	}
+
+	cookies.set('spotify', data['access_token'], {
+		path: '/',
+		httpOnly: true,
+		sameSite: 'strict',
+		secure: process.env.NODE_ENV === 'production',
+		maxAge: 60 * 55
 	});
 
 	throw redirect(301, '/connect');
