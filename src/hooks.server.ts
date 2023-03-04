b21143a1249738742ed32a114be6249d867c1e2a
Saketh Thota <saketht@umich.edu>
2023-03-04 14:15:53 -0600

    fixed protected routing in server side hook

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -14,16 +14,16 @@ const handleUser: Handle = async ({ event, resolve }) => {
 	event.locals.supabase = supabaseClient;
 	event.locals.session = session;
 
-	// if (event.locals.session) {
-	// 	if (event.url.pathname.startsWith('/login')) {
-	// 		throw redirect(302, '/');
-	// 	}
-	// 	if (event.url.pathname.startsWith('/register')) {
-	// 		throw redirect(302, '/');
-	// 	}
-	// } else {
-	// 	throw redirect(302, '/login');
-	// }
+	console.log(event.url.pathname);
+	if (event.locals.session) {
+		// logged in users can't access /login or /register until they logout
+		if (event.url.pathname.startsWith('/login')) throw redirect(302, '/');
+		if (event.url.pathname.startsWith('/register')) throw redirect(302, '/');
+	} else {
+		// not logged in users must login first
+		if (event.url.pathname !== '/login' && event.url.pathname !== '/register')
+			throw redirect(302, '/login');
+	}
 
 	return await resolve(event);
 };
